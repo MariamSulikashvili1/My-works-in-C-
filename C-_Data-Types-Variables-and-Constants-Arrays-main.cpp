9e63fd3d58e89d74eb1073c78e17a6354f4f8942
MariamSulikashvili1 <116154200+MariamSulikashvili1@users.noreply.github.com>
2023-01-11 17:57:03 +0400

    Add files via upload

---
+++ C-_Data-Types-Variables-and-Constants-Arrays-main.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+ 
+     
+{    char symbol = 'T';
+    cout<<symbol<< endl;
+    
+     
+    int number = 100;
+    cout<<number <<endl;
+    
+    
+    float first_number = 10.10;
+    cout<<first_number<<endl;
+    
+  
+    double second_number = 50.23;
+    cout<<second_number<<endl;
+    
+
+    long a = 5, b = 10, c = 15, d = 20, e = 30;
+    int sum = a + b + c + e;
+    int mult = a * b * c * e;
+    int average = (a + b + c + d + e) / 5;
+    
+     cout<<sum<<endl;
+     cout<<mult<<endl;
+     cout<<average<<endl;
+    
+      
+    
+    
+       return 0;
+
+}
